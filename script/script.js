783970cf8da0c1212e8051250198014d911435d8
Karambitos <pase4nikdv@gmail.com>
2019-10-29 21:40:47 +0200

    13

--- script/script.js
+++ script/script.js
@@ -7,15 +7,18 @@ document.addEventListener('DOMContentLoaded', () => {
     const services = document.querySelector('.servic_button')
     const servicesClose = document.querySelector('.services__close')
     const popupService = document.querySelector('.modal')
+    const spinner = document.querySelector('.spinner')
 
         services.addEventListener('click', (event) => {
             event.preventDefault();
             popupService.classList.add("modal-show");
+            spinner.style.display = 'none';
         });
 
         servicesClose.addEventListener('click', (event) => {
             event.preventDefault();
             popupService.classList.remove("modal-show");
+            spinner.style.display = '';
         });
 });
 
