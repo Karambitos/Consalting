11899675b4452d2f78352ad2b95b8566b104ad59
Karambitos <pase4nikdv@gmail.com>
2019-10-30 21:20:24 +0200

    1

--- script/script.js
+++ script/script.js
@@ -20,5 +20,35 @@ document.addEventListener('DOMContentLoaded', () => {
             popupService.classList.remove("modal-show");
             spinner.style.display = '';
         });
+
+
+    // BURGER
+
+    const nav = document.querySelector('.navigation')
+    const cont = document.querySelector('.contacts')
+    const siteNav = document.querySelector('.site-navigation')
+    const siteCont = document.querySelector('.site-contacts')
+    
+
+        spinner.addEventListener('click', (event) => {
+            event.preventDefault();
+            siteNav.classList.toggle("site-nav__active");
+            siteCont.classList.toggle("site-cont__active");
+            nav.classList.toggle("navigation__active");
+            cont.classList.toggle("contacts__active");
+
+            nav.style.display = 'block';
+            cont.style.display = 'block';
+
+            // if (nav.style.display = 'block') {
+            //     nav.style.display = ''
+            // } if (cont.style.display = 'block') {
+            //     cont.style.display = ''
+            // } 
+        });
+       
+    
 });
+
+
 
